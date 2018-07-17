09d833d9f5fc73b6cac2ce0856bc6cc75802d442
Joel Smith <joelwsmith123@gmail.com>
2018-07-17 17:12:49 -0600

    hover-button

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ submitButton.on("click", function(e) {
   var uniqueID = $.now();
   ideaSection.append(`<article class="idea" id="${uniqueID}">
                         <div class="top-line">
-                          <h3>${title}</h3>
-                          <input class="delete-btn small-btn" type="image" name="delete" src="images/delete.svg">
+                          <h3 contenteditable="true">${title}</h3>
+                          <input class="delete-btn small-btn" type="button" name="delete" src="images/delete.svg">
                         </div>
-                        <p>${body}</p>
+                        <p contenteditable="true">${body}</p>
                         <div class="bottom-line">
-                          <input class="upvote-btn small-btn bottom-btn" type="image" name="upvote" src="images/upvote.svg">
-                          <input class="downvote-btn small-btn bottom-btn" type="image" name="downvote" src="images/downvote.svg">
+                          <input class="upvote-btn small-btn bottom-btn" type="button" name="upvote">
+                          <input class="downvote-btn small-btn bottom-btn" type="button" name="downvote">
                           <h4>quality: <span class="quality">swill</span></h4>
                         </div>
                       </article>`);
