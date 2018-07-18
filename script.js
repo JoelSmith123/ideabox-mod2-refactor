b515edb1cd586f987e018d7f367965334d4ca33a
Joel Smith <joelwsmith123@gmail.com>
2018-07-18 17:20:06 -0600

    add local storage functionality

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ var ideaBody = $(".user-idea");
 var submitButton = $(".submit-btn");
 var ideaSection = $(".idea-section");
 var userSearch = $(".search-bar");
-var card = $(".idea");
+var allCards = Array(1);
+
+console.log(allCards);
 
 // ----------This is where the event listeners live----------
 
@@ -13,8 +15,12 @@ submitButton.on("click", function(e) {
   e.preventDefault();
   var title = ideaTitle.val();
   var body = ideaBody.val();
-  var uniqueID = $.now();
-  ideaSection.append(`<article class="idea" id="${uniqueID}">
+  var id = $.now();
+  var quality = "swill";
+  var newCard = $.makeArray(new Idea(id, title, body, quality));
+  $.merge(allCards, newCard);
+  localStorage.setItem("allCards", JSON.stringify(allCards));
+  ideaSection.append(`<article class="idea" id="${id}">
                         <div class="top-line">
                           <h3 contenteditable="true">${title}</h3>
                           <input class="delete-btn small-btn" type="button" name="delete" src="images/delete.svg">
@@ -23,16 +29,40 @@ submitButton.on("click", function(e) {
                         <div class="bottom-line">
                           <input class="upvote-btn small-btn bottom-btn" type="button" name="upvote">
                           <input class="downvote-btn small-btn bottom-btn" type="button" name="downvote">
-                          <h4>quality: <span class="quality">swill</span></h4>
+                          <h4>quality: <span class="quality">${quality}</span></h4>
                         </div>
                       </article>`);
   ideaTitle.val("");
   ideaBody.val("");
 });
 
+$(function() {
+  allCards = JSON.parse(localStorage.getItem("allCards"));
+  $.each(allCards, function() {
+    ideaSection.append(`<article class="idea" id="${this.id}">
+                          <div class="top-line">
+                            <h3 contenteditable="true">${this.title}</h3>
+                            <input class="delete-btn small-btn" type="button" name="delete" src="images/delete.svg">
+                          </div>
+                          <p contenteditable="true">${this.body}</p>
+                          <div class="bottom-line">
+                            <input class="upvote-btn small-btn bottom-btn" type="button" name="upvote">
+                            <input class="downvote-btn small-btn bottom-btn" type="button" name="downvote">
+                            <h4>quality: <span class="quality">${this.quality}</span></h4>
+                          </div>
+                        </article>`);
+  })
+})
+
 ideaSection.on("click", function(e) {
   if ($(e.target).hasClass("delete-btn")) {
     $(e.target).parent().parent().remove();
+    var tempID = $(e.target).parent().parent().attr("id");
+    $.each(allCards, function() {
+      if (tempID == this.id) {
+        
+      }
+    })
   } else if ($(e.target).hasClass("upvote-btn")) {
     if ($(e.target).siblings("h4").children("span").text() === "swill") {
       $(e.target).siblings("h4").children("span").text("plausible");
@@ -68,4 +98,9 @@ userSearch.on("keyup", function() {
 
 // ----------This is where the functions live----------
 
-
+function Idea (id, title, body, quality) {
+  this.id = id;
+  this.title = title;
+  this.body = body;
+  this.quality = quality;
+};
